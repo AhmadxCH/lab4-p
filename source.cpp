2bb27e8b0f7d4958aebd5655c7a2d90d37af258b
AhmadxCH <imahmad1414@gmail.com>
2024-10-28 13:00:49 +0500

    My 4th Repo

---
+++ source.cpp
@@ -0,0 +1,112 @@
+ task 18
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "**   **" << endl;
+	cout << "*******" << endl;
+	cout << "*******" << endl;
+	cout << "  ***  " << endl;
+	cout << "   *   " << endl;
+	return 0;
+ 
+}
+task 16
+{
+	cout << "25\3=" << 25 \ 3 << endl;
+	cout << "20-12\4*2=" << 20 - 12\4 * 2 << endl;
+	cout << "32%7=" << 32 % 7 << endl;
+	cout << "3-5%7=" << 3 - 5 % 7 << endl;
+	cout << "18.0\4=" << 18.0\4 << endl;
+	cout << "28-5\2.0=" << 28 - 5\2.0 << endl;
+	cout << "17+5%2-3=" << 17 + 5 % 2 - 3 << endl;
+	cout << "15.0+3.0*2.0\5.0" << endl;
+	return 0;
+}
+
+task 15
+
+{
+	cout << "                                 *****************                       " << endl;
+	cout << "                                   Result Sheet                         " << endl;
+	cout << "                                 *****************                       " << endl;
+	cout << "Student Roll No,      Math              Physics            Chemistry             Total" << endl;
+	cout << "______________________________________________________________________________________" << endl;
+	cout << "2357                   99                 85                   97                  281" << endl;
+	cout << "--------------------------------------------------------------------------------------" << endl;
+	return 0;
+}
+
+task 14
+{
+	cout << "2*1=2" << endl;
+	cout << "2*2=4" << endl;
+	cout << "2*3=6" << endl;
+	cout << "2*4=8" << endl;
+	cout << "2*5=10" << endl;
+	cout << "2*6=12" << endl;
+	cout << "2*7=14" << endl;
+	cout << "2*8=16" << endl;
+	cout << "2*9=18" << endl;
+	cout << "2*10=20" << endl;
+	return 0;
+}
+
+task 13
+
+{
+	cout << "CCCCCCCCCC               ++                 ++" << endl;
+	cout << "CC                       ++                 ++" << endl;
+	cout << "CC                 +++++++++++++       ++++++++++++ " << endl;
+	cout << "CC                 +++++++++++++       ++++++++++++ " << endl;
+	cout << "CC                       ++                 ++      " << endl;
+	cout << "CCCCCCCCCC               ++                 ++      " << endl;
+	return 0;
+
+}
+
+task 12
+
+{
+	cout << "********************************************" << endl;
+	cout << "*       Programming assignment 1           *" << endl;
+	cout << "*       Computer assignment  1             *" << endl;
+	cout << "*             Author:???                   *" << endl;
+	cout << "*       Due Date:Thursday,January 24       *" << endl;
+	cout << "********************************************" << endl;
+	return 0;
+
+}
+
+task 11
+{
+
+	cout << " ***" << endl;
+	cout << "*****" << endl;
+	cout << "  *  " << endl;
+	cout << "  *  " << endl;
+	return 0;
+}
+
+task 10
+
+{
+	cout << "***" << endl;
+	cout << "* *" << endl;
+	cout << "***" << endl;
+	return 0;
+}
+
+task 9
+
+{
+	cout << "   *   " << endl;
+	cout << "*******" << endl;
+	cout << "   *   " << endl;
+	return 0;
+}
+
+task 17
